005ce0999163e30f18805283f6939cdf3f1c67c7
unknown <jamie.gworld@hotmail.com>
2023-04-06 17:08:58 +0700

    OK

--- srcs/execution/expend.c
+++ srcs/execution/expend.c
@@ -12,6 +12,7 @@
 
 #include "../../includes/minishell.h"
 
+//meme principe que expend_str pour une liste
 int	expend_list(t_envi *envi, t_list *list, int exit_value)
 {
 	t_list	*lst;
@@ -34,6 +35,8 @@ int	expend_list(t_envi *envi, t_list *list, int exit_value)
 	return (EXIT_SUCCESS);
 }
 
+//expend pour les variables speciales
+//ex: $1, $$, ...
 int	expend_exit(char *str, t_list **join, int exit_value)
 {
 	char	*to_join;
@@ -61,7 +64,7 @@ int	expend_exit(char *str, t_list **join, int exit_value)
 //puis retourner la variable 
 //ex:	on a e$A avec $A = cho
 //		expend_join va joindre e avec $A
-//		on obtient ainsi e$A = echo
+//		on obtient ainsi e$A = echoz
 char	*expend_join(t_envi *envi, t_list **join, char *str)
 {
 	size_t	i;
